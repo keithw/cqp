cc93fb3a84939dda5fd4042866aa4c8b32b5602a
Keith Winstein <keithw@cs.stanford.edu>
2015-09-22 03:33:41 -0700

    Import fader from Ratatouille

--- src/cqp.cc
+++ src/cqp.cc
@@ -1,4 +1,24 @@
+#include <cstdlib>
+#include <thread>
+#include <chrono>
+
+#include <unistd.h>
+
+#include "fader.hh"
+
+using namespace std;
+
 int main()
 {
-  return 0;
+  GTKFader fader { 5 };
+
+  while ( true ) {
+    this_thread::sleep_for( chrono::milliseconds( 500 ) );
+
+    if ( fader.quit() ) {
+      break;
+    }
+  }
+
+  return EXIT_SUCCESS;
 }
